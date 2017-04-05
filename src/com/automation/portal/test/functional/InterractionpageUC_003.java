a0fd197d3982cd2e0c901acc4ed2625680f84278
qatesting30 <qatesting30@gmail.com>
2017-04-05 20:20:47 +0530

    c

--- src/com/automation/portal/test/functional/InterractionpageUC_003.java
+++ src/com/automation/portal/test/functional/InterractionpageUC_003.java
@@ -2,27 +2,66 @@ package com.automation.portal.test.functional;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.automation.portal.generic.utils.SwitchTo;
+import com.automation.portal.generic.utils.signIn;
+import com.automation.portal.ui.DraggablePage;
+import com.automation.portal.ui.ResizablePage;
 
 public class InterractionpageUC_003 {
 	
 	public static  WebDriver driver;
 	public static WebDriverWait wait;
+	public static DraggablePage drp;
+	public static ResizablePage rp;
 	
 	@BeforeTest
 	public static void beforeresizable()
 	{
 		InterractionPageUC_001.beforeHomePage();
 		driver = InterractionPageUC_001.driver;
+		drp = new DraggablePage(driver);
+		rp = new ResizablePage(driver);
 	}
-	@Test
-	public static void resizableTC_001()
+	@Test(priority=1)
+	public static void resizableTC_001() throws Exception
 	{
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		wait = new WebDriverWait(driver, 30);
+		Actions act = new Actions(driver);
+		wait = new WebDriverWait(driver, 20);
+		act.moveToElement(drp.interactionTab()).perform();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[contains(@style,'display: block')]//a[contains(text(),'Resizable')]"))).click();
+		SwitchTo.windowsAndTabs(driver, 1);
+		signIn.autoSignIn(driver);
+		Thread.sleep(4000);
+		act.moveToElement(drp.interactionTab()).perform();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[contains(@style,'display: block')]//a[contains(text(),'Resizable')]"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(rp.resizable_default_functionality())).click();
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(rp.resizable_frame_default_functionality()));
+		act.clickAndHold(rp.resizable_default_functionality_resizable_item_ht()).moveByOffset(100, 0).release().perform();
+		driver.switchTo().defaultContent();
+		
 	}
-
+	@Test(priority=2)
+	public static void resizableTC_002() throws Exception
+	{
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 30);
+		Actions act = new Actions(driver);
+		wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.elementToBeClickable(rp.resizable_max_min())).click();
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(rp.resizable_frame_max_min()));
+		act.clickAndHold(rp.resizable_max_min_resizable_item_ht()).moveByOffset(100, 0).release().perform();
+		act.clickAndHold(rp.resizable_max_min_resizable_item_wdt()).moveByOffset(0, 100).release().perform();
+		
+	}
+	
 }
--- src/com/automation/portal/ui/ResizablePage.java
+++ src/com/automation/portal/ui/ResizablePage.java
@@ -1,5 +1,6 @@
 package com.automation.portal.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -12,6 +13,39 @@ public class ResizablePage {
 		this.driver=driver;
 	}
 	
-	//public WebElement 
+
+	public WebElement resizable_default_functionality()
+	{
+		return driver.findElement(By.xpath("//a[@href='#example-1-tab-1']"));
+	}
+	public WebElement resizable_max_min()
+	{
+		return driver.findElement(By.xpath("//a[@href='#example-1-tab-5']"));
+	}
+	public WebElement resizable_frame_default_functionality()
+	{
+		return driver.findElement(By.xpath("//iframe[@src='resizable/default.html']"));
+	}
+	public WebElement resizable_frame_max_min()
+	{
+		return driver.findElement(By.xpath("//iframe[@src='resizable/default5.html']"));
+	}
+	public WebElement resizable_default_functionality_resizable_item_ht()
+	{
+		return driver.findElement(By.xpath("//div[@class='ui-resizable-handle ui-resizable-e']"));
+	}
+	public WebElement resizable_default_functionality_resizable_item_wdt()
+	{
+		return driver.findElement(By.xpath("//div[@class='ui-resizable-handle ui-resizable-s']"));
+	}
+	public WebElement resizable_max_min_resizable_item_ht()
+	{
+		return driver.findElement(By.xpath("//div[@class='ui-resizable-handle ui-resizable-e']"));
+	}
+	public WebElement resizable_max_min_resizable_item_wdt()
+	{
+		return driver.findElement(By.xpath("//div[@class='ui-resizable-handle ui-resizable-s']"));
+	}
+	
 
 }
